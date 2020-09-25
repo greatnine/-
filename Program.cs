e8f0f6038a907d9aa104b3970366325b62269dba
greatnine <qijiuzhu@hotmail.com>
2020-09-25 17:33:09 +0800

    添加项目文件。

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿/*****************************************************************************
+* @Author: 传道者@戚
+* @Contact: Email:qijiuzhu@tsinghua.org.cn; QQ:93022804; Wechat:Aldrich_Tsee
+* @Created 2020/8/28 12:10:07
+* @Description: MySolution1  Program
+******************************************************************************/
+
+//Console.WriteLine("\r\n//MyBestLove\r\n");
+
+using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace 文心雕龙
+{
+    partial class Program
+    {
+        public static void Main()
+        {
+            //MessageBox.Show("");
+
+            文心雕龙.文本模板.运行时模板.Run();
+
+
+
+            //
+            Console.WriteLine("输出窗口");
+            Console.ReadLine();
+          
+        }
+    }
+}
+
+
---
+++ 文本模板/运行时模板.tt.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 文心雕龙.文本模板
+{
+    public partial class 运行时模板
+    {
+        public static void Run()
+        {
+            string Content = new 运行时模板().TransformText();
+
+            //Debug.WriteLine("显示结果");
+            Debug.WriteLine("---------");
+            Debug.WriteLine(Content);
+           
+            //ConsoleX.WriteLine(Content);
+            //ConsoleX.Show();
+        }
+    }
+}
+
+public class AppSetting
+{
+    public static string AppName = "文心雕龙";
+}
+
+/*
+
+<#=AppSetting.AppName#>
+
+*/
